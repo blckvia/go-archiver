7a30c0c7417a9e2fa6c52f50888cd5a110f6419c
blckvia <a.vyatkinn@icloud.com>
2023-12-10 22:01:57 +0300

    splitByChunks and encodeBin func and test for them has been added

--- lib/vlc/vlc.go
+++ lib/vlc/vlc.go
@@ -1,21 +1,120 @@
 package vlc
 
 import (
+	"fmt"
 	"strings"
 	"unicode"
+	"unicode/utf8"
 )
+
+type BinaryChunks []BinaryChunk
+
+type BinaryChunk string
+
+type encodingTable map[rune]string
+
+const chunkSize = 8
 
 func Encode(str string) string {
 	str = prepareText(str)
 
-	// some text -> 00101001
+	bStr := encodeBin(str)
 
-	// split binary by chunks
+	chunks := splitByChunks(bStr, chunkSize)
+
+	fmt.Println(chunks)
 
 	// bytes to hex
 
 	// return string
 	return ""
+}
+
+// splitByChunks splits binary string by chunks with given size,
+func splitByChunks(bStr string, chunkSize int) BinaryChunks {
+	strLen := utf8.RuneCountInString(bStr)
+
+	chunksCount := strLen / chunkSize
+
+	if strLen/chunkSize != 0 {
+		chunksCount++
+	}
+
+	res := make(BinaryChunks, 0, chunksCount)
+
+	var buf strings.Builder
+
+	for i, ch := range bStr {
+		buf.WriteString(string(ch))
+
+		if (i+1)%chunkSize == 0 {
+			res = append(res, BinaryChunk(buf.String()))
+			buf.Reset()
+		}
+	}
+	if buf.Len() != 0 {
+		lastChunk := buf.String()
+
+		lastChunk += strings.Repeat("0", chunkSize-len(lastChunk))
+
+		res = append(res, BinaryChunk(lastChunk))
+	}
+
+	return res
+}
+
+// encodeBin encodes str into binary codes string without spaces.
+func encodeBin(str string) string {
+	var buf strings.Builder
+
+	for _, ch := range str {
+		buf.WriteString(bin(ch))
+	}
+	return buf.String()
+}
+
+func bin(ch rune) string {
+	table := getEncodingTable()
+
+	res, ok := table[ch]
+	if !ok {
+		panic("unknown character" + string(ch))
+	}
+
+	return res
+}
+
+func getEncodingTable() encodingTable {
+	return encodingTable{
+		' ': "11",
+		't': "1001",
+		'n': "10000",
+		's': "0101",
+		'r': "01000",
+		'd': "00101",
+		'!': "001000",
+		'c': "000101",
+		'm': "000011",
+		'g': "0000100",
+		'b': "0000010",
+		'v': "00000001",
+		'k': "0000000001",
+		'q': "000000000001",
+		'e': "101",
+		'o': "10001",
+		'a': "011",
+		'i': "01001",
+		'h': "0011",
+		'l': "001001",
+		'u': "00011",
+		'f': "000100",
+		'p': "0000101",
+		'w': "0000011",
+		'y': "0000001",
+		'j': "000000001",
+		'x': "00000000001",
+		'z': "000000000000",
+	}
 }
 
 // prepateText prepares text to be fir for encode:
--- lib/vlc/vlc_test.go
+++ lib/vlc/vlc_test.go
@@ -1,6 +1,9 @@
 package vlc
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_prepareText(t *testing.T) {
 	tests := []struct {
@@ -18,6 +21,55 @@ func Test_prepareText(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := prepareText(tt.str); got != tt.want {
 				t.Errorf("prepareTest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_encodeBin(t *testing.T) {
+	tests := []struct {
+		name string
+		str  string
+		want string
+	}{
+		{
+			name: "base test",
+			str:  "!ted",
+			want: "001000100110100101",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := encodeBin(tt.str); got != tt.want {
+				t.Errorf("encodeBin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_splitByChunks(t *testing.T) {
+	type args struct {
+		bStr      string
+		chunkSize int
+	}
+	tests := []struct {
+		name string
+		args args
+		want BinaryChunks
+	}{
+		{
+			name: "base test",
+			args: args{
+				bStr:      "001000100110100101",
+				chunkSize: 8,
+			},
+			want: BinaryChunks{"00100010", "01101001", "01000000"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := splitByChunks(tt.args.bStr, tt.args.chunkSize); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("splitByChunks() = %v, want %v", got, tt.want)
 			}
 		})
 	}
